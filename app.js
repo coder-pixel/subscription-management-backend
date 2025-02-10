18e7dffef06aa61500e5eff445ce92c76fef09c4
sauvik <sauvik@logic-square.com>
2025-02-10 17:26:05 +0530

    feat: initial setup done

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}`);
+});
+
+export default app;
